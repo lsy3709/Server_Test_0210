2f9bed03932e9b37fb3c88d251fef8871fa8d959
leejaeuk0718 <jaeuk2210@naver.com>
2026-02-10 14:20:07 +0900

    이재욱 제출 pr

---
+++ src/main/java/com/busanit501/server_test_0210/_0209_todo/domain/_0209_5_TodoVO.java
@@ -0,0 +1,16 @@
+package com.busanit501.server_test_0210._0209_todo.domain;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor // 기본 생성자를 생성함.
+@AllArgsConstructor // 모든 멤버를 매개변수로 가지는 생성자를 생성함.
+public class _0209_5_TodoVO {
+
+    private Long tno;
+    private String title;
+    private LocalDate dueDate;
+    private  boolean finished;
+}
